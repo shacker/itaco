610c634336610930f7d6a4aa3113a6e149abf4bf
Scot Hacker <shacker@birdhouse.org>
2011-09-09 00:20:27 -0700

    Add new static directory.

---
+++ static/js/selectobject.js
@@ -0,0 +1,10 @@
+function YearSelect(){
+        if (document.Links.SelectYear.options[document.Links.SelectYear.selectedIndex].value != "none"){
+        location = document.Links.SelectYear.options[document.Links.SelectYear.selectedIndex].value}
+}
+
+function PeriodSelect(){
+        if (document.Links.SelectPeriod.options[document.Links.SelectPeriod.selectedIndex].value != "none"){
+        location = document.Links.SelectPeriod.options[document.Links.SelectPeriod.selectedIndex].value}
+}
+

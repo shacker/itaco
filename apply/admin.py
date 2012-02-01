b3b55de0c1a8d9d6f8b575276d76e9683175641a
Scot Hacker <shacker@birdhouse.org>
2012-01-31 19:55:33 -0800

    Add eval_date column to admin for Application model.

--- apply/admin.py
+++ apply/admin.py
@@ -4,6 +4,6 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib import messages
 
 class ApplicationAdmin(admin.ModelAdmin):
-    list_display = ('child_last','child_first','appdate','rq_start_date')
+    list_display = ('child_last','child_first','appdate','rq_start_date','eval_date')
 
 admin.site.register(Application, ApplicationAdmin)

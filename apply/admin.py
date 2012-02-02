0f33a42c014b2c1de864db163b27823cef72ee4b
Scot Hacker <shacker@birdhouse.org>
2012-02-02 00:20:24 -0800

    Added list_filter to admin for applications. Linked to same from applications list.

--- apply/admin.py
+++ apply/admin.py
@@ -4,6 +4,7 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib import messages
 
 class ApplicationAdmin(admin.ModelAdmin):
-    list_display = ('child_last','child_first','appdate','rq_start_date','eval_date')
+    list_display = ('child_last','child_first','grade','appdate','rq_start_date','eval_date')
+    list_filter = ('grade',)
 
 admin.site.register(Application, ApplicationAdmin)

b6534b0d4f68fa1693fd73f3faa7e754e94e95af
Scot Hacker <shacker@birdhouse.org>
2012-07-26 08:29:28 -0700

    Make sure we don't die from "No current year" during the transition between school years.

--- itaco/context_processors.py
+++ itaco/context_processors.py
@@ -13,16 +13,29 @@ def family_id(request):
         myfam_id=p.family.id
     except:
         myfam_id=None
-        
+
     return {
         'myfam_id': myfam_id,
     }
 
 def cur_year(request):
     '''
-    Discover current school year 
+    Discover current school year
     '''
+    try:
+        cur_year = SchoolYear.objects.get(current=True)
+    except:
+        '''
+        Not ideal to fall back on a particular year like this,
+        but many parts of the site rely on there being a cur_year
+        available everywhere, and by picking one we can at least
+        still access the admin. This will only ever kick in
+        if someone sets NO current year in the admin, which is
+        briefly required during the transition between years.
+        '''
+        cur_year = SchoolYear.objects.get(title='2011 - 2012')
+
     return {
-        'cur_year': SchoolYear.objects.get(current=True),
+        'cur_year':  cur_year,
     }
 
